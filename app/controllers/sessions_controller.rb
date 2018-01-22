c688f5e2359383f6fc2470a2070deb90ea008b4f
Anh Quan <anhquan.nguyenle@gmai.com>
2018-01-22 13:31:59 +0700

    chap-8

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    session = params[:session]
+    user = User.find_by email: session[:email].downcase
+    if user && user.authenticate(session[:password])
+      log_in_and_redicrect user
+    else
+      flash.now[:danger] = t "flash.danger"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+  private
+
+  def log_in_and_redicrect user
+    log_in user
+    redirect_to user
+  end
+
+  def log_out
+    session.delete :user_id
+    @current_user = nil
+  end
+end
